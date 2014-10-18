79b51fcbbc5e11ee529fe5237350e8f01fbb217e
ZahidHasan4mJU <responsible.zahid@yahoo.com>
2014-10-18 22:03:34 +0600

    added

--- StudentResultInfoApp/UI/ResultEntryUI.Designer.cs
+++ StudentResultInfoApp/UI/ResultEntryUI.Designer.cs
@@ -28,19 +28,190 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.saveButtonInResultEntry = new System.Windows.Forms.Button();
+            this.viewResultSheetButton = new System.Windows.Forms.Button();
+            this.findButtonInResultEntry = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.percentScoreTextBox = new System.Windows.Forms.TextBox();
+            this.studentCorurseComboBoxinResultEntry = new System.Windows.Forms.ComboBox();
+            this.studentEmailTextboxInResultSheet = new System.Windows.Forms.TextBox();
+            this.studentNameTextBoxInResultEntry = new System.Windows.Forms.TextBox();
+            this.studentRegNoTextBoxInResultEntry = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // saveButtonInResultEntry
+            // 
+            this.saveButtonInResultEntry.Location = new System.Drawing.Point(307, 142);
+            this.saveButtonInResultEntry.Name = "saveButtonInResultEntry";
+            this.saveButtonInResultEntry.Size = new System.Drawing.Size(98, 23);
+            this.saveButtonInResultEntry.TabIndex = 29;
+            this.saveButtonInResultEntry.Text = "Save";
+            this.saveButtonInResultEntry.UseVisualStyleBackColor = true;
+            // 
+            // viewResultSheetButton
+            // 
+            this.viewResultSheetButton.Location = new System.Drawing.Point(307, 6);
+            this.viewResultSheetButton.Name = "viewResultSheetButton";
+            this.viewResultSheetButton.Size = new System.Drawing.Size(134, 23);
+            this.viewResultSheetButton.TabIndex = 28;
+            this.viewResultSheetButton.Text = "View Result Sheet";
+            this.viewResultSheetButton.UseVisualStyleBackColor = true;
+            // 
+            // findButtonInResultEntry
+            // 
+            this.findButtonInResultEntry.Location = new System.Drawing.Point(203, 6);
+            this.findButtonInResultEntry.Name = "findButtonInResultEntry";
+            this.findButtonInResultEntry.Size = new System.Drawing.Size(98, 23);
+            this.findButtonInResultEntry.TabIndex = 27;
+            this.findButtonInResultEntry.Text = "Find";
+            this.findButtonInResultEntry.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(4, 141);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(81, 13);
+            this.label6.TabIndex = 26;
+            this.label6.Text = "Publishing Date";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(95, 141);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 25;
+            // 
+            // percentScoreTextBox
+            // 
+            this.percentScoreTextBox.Location = new System.Drawing.Point(92, 112);
+            this.percentScoreTextBox.Name = "percentScoreTextBox";
+            this.percentScoreTextBox.Size = new System.Drawing.Size(105, 20);
+            this.percentScoreTextBox.TabIndex = 24;
+            // 
+            // studentCorurseComboBoxinResultEntry
+            // 
+            this.studentCorurseComboBoxinResultEntry.FormattingEnabled = true;
+            this.studentCorurseComboBoxinResultEntry.Location = new System.Drawing.Point(92, 84);
+            this.studentCorurseComboBoxinResultEntry.Name = "studentCorurseComboBoxinResultEntry";
+            this.studentCorurseComboBoxinResultEntry.Size = new System.Drawing.Size(165, 21);
+            this.studentCorurseComboBoxinResultEntry.TabIndex = 23;
+            // 
+            // studentEmailTextboxInResultSheet
+            // 
+            this.studentEmailTextboxInResultSheet.Location = new System.Drawing.Point(92, 58);
+            this.studentEmailTextboxInResultSheet.Name = "studentEmailTextboxInResultSheet";
+            this.studentEmailTextboxInResultSheet.Size = new System.Drawing.Size(165, 20);
+            this.studentEmailTextboxInResultSheet.TabIndex = 22;
+            // 
+            // studentNameTextBoxInResultEntry
+            // 
+            this.studentNameTextBoxInResultEntry.Location = new System.Drawing.Point(92, 32);
+            this.studentNameTextBoxInResultEntry.Name = "studentNameTextBoxInResultEntry";
+            this.studentNameTextBoxInResultEntry.Size = new System.Drawing.Size(349, 20);
+            this.studentNameTextBoxInResultEntry.TabIndex = 21;
+            // 
+            // studentRegNoTextBoxInResultEntry
+            // 
+            this.studentRegNoTextBoxInResultEntry.Location = new System.Drawing.Point(92, 6);
+            this.studentRegNoTextBoxInResultEntry.Name = "studentRegNoTextBoxInResultEntry";
+            this.studentRegNoTextBoxInResultEntry.Size = new System.Drawing.Size(105, 20);
+            this.studentRegNoTextBoxInResultEntry.TabIndex = 20;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(36, 112);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 13);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "Score(%)";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(36, 87);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Course";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(36, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Email";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Name";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Reg No";
             // 
             // ResultEntryUI
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(759, 443);
+            this.ClientSize = new System.Drawing.Size(490, 202);
+            this.Controls.Add(this.saveButtonInResultEntry);
+            this.Controls.Add(this.viewResultSheetButton);
+            this.Controls.Add(this.findButtonInResultEntry);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.percentScoreTextBox);
+            this.Controls.Add(this.studentCorurseComboBoxinResultEntry);
+            this.Controls.Add(this.studentEmailTextboxInResultSheet);
+            this.Controls.Add(this.studentNameTextBoxInResultEntry);
+            this.Controls.Add(this.studentRegNoTextBoxInResultEntry);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Name = "ResultEntryUI";
             this.Text = "ResultEntryUI";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button saveButtonInResultEntry;
+        private System.Windows.Forms.Button viewResultSheetButton;
+        private System.Windows.Forms.Button findButtonInResultEntry;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox percentScoreTextBox;
+        private System.Windows.Forms.ComboBox studentCorurseComboBoxinResultEntry;
+        private System.Windows.Forms.TextBox studentEmailTextboxInResultSheet;
+        private System.Windows.Forms.TextBox studentNameTextBoxInResultEntry;
+        private System.Windows.Forms.TextBox studentRegNoTextBoxInResultEntry;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
