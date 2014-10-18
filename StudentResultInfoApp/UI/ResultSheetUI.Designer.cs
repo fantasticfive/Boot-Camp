18c412a1757de84d25ee3bcc322360c26528a3d1
Mehedee-Hassan <razin.meha@gmail.com>
2014-10-18 23:19:08 +0600

    v2 ui2

--- StudentResultInfoApp/UI/ResultSheetUI.Designer.cs
+++ StudentResultInfoApp/UI/ResultSheetUI.Designer.cs
@@ -28,19 +28,182 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.resultSheetListView = new System.Windows.Forms.ListView();
+            this.findButtonInResultSheet = new System.Windows.Forms.Button();
+            this.gradeLetterTextBox = new System.Windows.Forms.TextBox();
+            this.averageScoreTextBoxInResultSheet = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.studentEmailTextBoxInResultSheet = new System.Windows.Forms.TextBox();
+            this.studentNameTextBoxInResultSheet = new System.Windows.Forms.TextBox();
+            this.studentRegNoTextBoxInResultSheet = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.resultSheetListView);
+            this.groupBox2.Location = new System.Drawing.Point(14, 106);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(530, 152);
+            this.groupBox2.TabIndex = 17;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Result";
+            // 
+            // resultSheetListView
+            // 
+            this.resultSheetListView.Location = new System.Drawing.Point(10, 19);
+            this.resultSheetListView.Name = "resultSheetListView";
+            this.resultSheetListView.Size = new System.Drawing.Size(507, 121);
+            this.resultSheetListView.TabIndex = 13;
+            this.resultSheetListView.UseCompatibleStateImageBehavior = false;
+            // 
+            // findButtonInResultSheet
+            // 
+            this.findButtonInResultSheet.Location = new System.Drawing.Point(201, 13);
+            this.findButtonInResultSheet.Name = "findButtonInResultSheet";
+            this.findButtonInResultSheet.Size = new System.Drawing.Size(80, 23);
+            this.findButtonInResultSheet.TabIndex = 12;
+            this.findButtonInResultSheet.Text = "Find";
+            this.findButtonInResultSheet.UseVisualStyleBackColor = true;
+            // 
+            // gradeLetterTextBox
+            // 
+            this.gradeLetterTextBox.Location = new System.Drawing.Point(385, 36);
+            this.gradeLetterTextBox.Name = "gradeLetterTextBox";
+            this.gradeLetterTextBox.Size = new System.Drawing.Size(134, 20);
+            this.gradeLetterTextBox.TabIndex = 11;
+            // 
+            // averageScoreTextBoxInResultSheet
+            // 
+            this.averageScoreTextBoxInResultSheet.Location = new System.Drawing.Point(407, 10);
+            this.averageScoreTextBoxInResultSheet.Name = "averageScoreTextBoxInResultSheet";
+            this.averageScoreTextBoxInResultSheet.Size = new System.Drawing.Size(112, 20);
+            this.averageScoreTextBoxInResultSheet.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(309, 36);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(66, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Grade Letter";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(309, 13);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(92, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Average Score(%)";
+            // 
+            // studentEmailTextBoxInResultSheet
+            // 
+            this.studentEmailTextBoxInResultSheet.Location = new System.Drawing.Point(61, 62);
+            this.studentEmailTextBoxInResultSheet.Name = "studentEmailTextBoxInResultSheet";
+            this.studentEmailTextBoxInResultSheet.Size = new System.Drawing.Size(134, 20);
+            this.studentEmailTextBoxInResultSheet.TabIndex = 7;
+            // 
+            // studentNameTextBoxInResultSheet
+            // 
+            this.studentNameTextBoxInResultSheet.Location = new System.Drawing.Point(61, 36);
+            this.studentNameTextBoxInResultSheet.Name = "studentNameTextBoxInResultSheet";
+            this.studentNameTextBoxInResultSheet.Size = new System.Drawing.Size(220, 20);
+            this.studentNameTextBoxInResultSheet.TabIndex = 6;
+            // 
+            // studentRegNoTextBoxInResultSheet
+            // 
+            this.studentRegNoTextBoxInResultSheet.Location = new System.Drawing.Point(61, 13);
+            this.studentRegNoTextBoxInResultSheet.Name = "studentRegNoTextBoxInResultSheet";
+            this.studentRegNoTextBoxInResultSheet.Size = new System.Drawing.Size(134, 20);
+            this.studentRegNoTextBoxInResultSheet.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 65);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Email";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Name";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Reg No.";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.findButtonInResultSheet);
+            this.groupBox1.Controls.Add(this.gradeLetterTextBox);
+            this.groupBox1.Controls.Add(this.averageScoreTextBoxInResultSheet);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.studentEmailTextBoxInResultSheet);
+            this.groupBox1.Controls.Add(this.studentNameTextBoxInResultSheet);
+            this.groupBox1.Controls.Add(this.studentRegNoTextBoxInResultSheet);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(533, 90);
+            this.groupBox1.TabIndex = 16;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Student Information";
             // 
             // ResultSheetUI
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(726, 413);
+            this.ClientSize = new System.Drawing.Size(565, 277);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Name = "ResultSheetUI";
             this.Text = "ResultSheetUI";
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListView resultSheetListView;
+        private System.Windows.Forms.Button findButtonInResultSheet;
+        private System.Windows.Forms.TextBox gradeLetterTextBox;
+        private System.Windows.Forms.TextBox averageScoreTextBoxInResultSheet;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox studentEmailTextBoxInResultSheet;
+        private System.Windows.Forms.TextBox studentNameTextBoxInResultSheet;
+        private System.Windows.Forms.TextBox studentRegNoTextBoxInResultSheet;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
