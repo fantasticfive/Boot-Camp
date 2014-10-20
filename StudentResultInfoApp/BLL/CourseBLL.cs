fdc344da2336e97ff2757d97d01a2b3ffd04866b
acmovi <acm24.cse@gmail.com>
2014-10-20 15:33:47 +0600

    v9u8

--- StudentResultInfoApp/BLL/CourseBLL.cs
+++ StudentResultInfoApp/BLL/CourseBLL.cs
@@ -26,5 +26,48 @@ namespace StudentResultInfoApp.BLL
             courses = aCourseGateWay.ShoInListViewFromDB(aStudent);
             return courses;
         }
+
+        public void DoWrong()
+        {
+             public double CalculateAvg(Student aStudent)
+        {
+            aResultGateWay = new ResultGateWay();
+            List<double> allResult = new List<double>();
+
+            
+            allResult = aResultGateWay.GetAllScore(aStudent);
+
+            double total = 0;
+
+            foreach (double d in allResult)
+            {
+            total += d;
+            }
+
+
+            double avgResult = total/allResult.Count;
+
+
+            return avgResult;
+        }
+
+        public string CalculateGrade(double score)
+        {
+            if (score >= 90)
+                return "A";
+            else if(score >= 70 && score < 90)
+            {
+                return "B";
+            }
+            else if (score >= 50 && score < 70)
+            {
+                return "C";
+            }
+            else
+            {
+                return "F";
+            }
+        }
+        }
     }
 }
